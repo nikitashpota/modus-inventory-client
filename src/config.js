ac1dd77e427abda68d79b995bc066552f0c0c25b
nikitashpota <nikitashpota@ya.ru>
2023-01-11 15:34:38 +0300

    cm

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://45.130.43.181:8080";
-export const CLIENT_URL = "http://45.130.43.181:3000";
+export const API_URL = "http://62.117.108.6:8080";
+export const CLIENT_URL = "http://62.117.108.6:3000";
+
